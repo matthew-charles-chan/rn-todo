ec6630d82158ebaf85caf78f47accd9672543bb4
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-09-28 15:45:33 -0700

    add "add toDo input"

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>GOOD BYE</Text>
-      <StatusBar style="auto" />
+    <View  style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="ToDo Item"
+          style={styles.textInput}
+        />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  }
+})
